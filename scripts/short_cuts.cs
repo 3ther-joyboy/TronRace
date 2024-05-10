a262851dce85860e307dda42103c8e2ec9c9b857
3ther-joyboy <3ther.not.found@gmail.com>
2024-05-10 11:36:49 +0200

    restart on touch?

--- scripts/short_cuts.cs
+++ scripts/short_cuts.cs
@@ -4,9 +4,20 @@ using System;
 public partial class short_cuts : Node
 {
 	public override void _UnhandledInput(InputEvent @event){
-		if (@event is InputEventKey eventKey && eventKey.Pressed){
+
+		if (@event is InputEventKey eventKey && eventKey.Pressed) {
 			if (eventKey.Keycode == (Key)4194336) // f5 tho idk how to acces Key. object
-				GetTree().ReloadCurrentScene();
+				_Restart();
 		}
+
+		if (@event is InputEventScreenTouch eventTouch) {
+			if (eventTouch.Index == 3)
+				_Restart();
+			
+		}
+	}
+
+	private void _Restart() {
+		GetTree().ReloadCurrentScene();
 	}
 }
