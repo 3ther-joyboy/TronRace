451fc1a51da60d2b068486462a99c9460821b11e
3ther-joyboy <3ther.not.found@gmail.com>
2024-05-10 08:42:23 +0200

    smooth camera [franta aproofed]

--- scripts/moving_camera.cs
+++ scripts/moving_camera.cs
@@ -3,10 +3,12 @@ using System;
 
 public partial class moving_camera : Camera2D
 {
-	[Export(PropertyHint.Range,"0,10,or_grater")]
+	[Export(PropertyHint.Range,"0,1000")]
 	private int _speed = 100;
+	[Export(PropertyHint.Range,"0,1")]
+	private float _offset = 0.2f;
 	[Export(PropertyHint.Range,"0,2,or_greater")]
-	private float _zoom = 1f;
+	private float _zoom = 0.5f;
 
 	private move_component _follower;
 
@@ -15,7 +17,7 @@ public partial class moving_camera : Camera2D
 	}
 
 	public override void _PhysicsProcess(double delta) {
-		this.Position += this.Position.DirectionTo( _follower.LinearVelocity/10 ) * _speed * (float)delta;
+		this.Position += this.Position.DirectionTo( _follower.LinearVelocity * _offset ) * _speed * (float)delta;
 
 		this.Zoom = Vector2.One - ( Vector2.One * this.Position.Length()/_follower.GetAcceleration() ) * _zoom;
 	}
---
+++ scripts/short_cuts.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class short_cuts : Node
+{
+	public override void _UnhandledInput(InputEvent @event){
+
+	}
+}
