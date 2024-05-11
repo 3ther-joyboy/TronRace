e41cad86bb20fa8f4681a8c8503d188ed29c783a
3ther-joyboy <3ther.not.found@gmail.com>
2024-05-11 20:01:50 +0200

    audio buses set as defauld

--- scripts/autoload.cs
+++ scripts/autoload.cs
@@ -5,30 +5,31 @@ using System.Text.Json;
 public partial class autoload : Node
 {
 	private String path = "res://replay_test.json";
-//	public String replayJson = ""; //FileAccess.Open("res://replay_test.json", FileAccess.ModeFlags.Read).GetAsText();
-	
+	//public String replayJson = FileAccess.Open("res://replay_test.json", FileAccess.ModeFlags.Read).GetAsText();
+
 	public override void _Ready(){
 
 	}
 
 	public RecordFormat[] GetReplay() {
-//		JsonElement replay = JsonDocument.Parse(replayJson).RootElement;
+//		using JsonDocument doc = JsonDocument.Parse(replayJson);
+//		JsonElement replay = doc.RootElement;
 //
 //		RecordFormat[] recording = {};
 //		for (int i = 0; i < replay.GetArrayLength(); i++)
 //			recording[i] = new RecordFormat(replay[i]);
-//		
+//
 //
 		return null;
 	}
 	public void SaveReplay(RecordFormat[] replay) {
 		GD.Print("Saving Replay");
 
- 		using var file = FileAccess.Open(path, FileAccess.ModeFlags.Write);
- 		String objs = "";
- 		for (int i = 0; i < replay.Length; i++)
- 			objs += "{\"time\":" + replay[i].time + ", \"dir\": [" + replay[i].dir.X + "," + replay[i].dir.Y + "]},";
- 		file.StoreLine("[" + objs + "]");
+		using var file = FileAccess.Open(path, FileAccess.ModeFlags.Write);
+		String objs = "";
+		for (int i = 0; i < replay.Length; i++)
+			objs += "{\"time\":" + replay[i].time + ", \"dir\": [" + replay[i].dir.X + "," + replay[i].dir.Y + "]},";
+		file.StoreString("[" + objs + "]");
 
 		GD.Print("Replay Saved");
 
