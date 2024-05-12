5264cc854e744f0f379ff9a425acabeafc1781ea
Frantisek Lednicky <frantisek.ledicky@outlook.com>
2024-05-12 19:12:14 +0200

    actually funkcni menu

--- scripts/ui/pause.cs
+++ scripts/ui/pause.cs
@@ -1,19 +1,31 @@
 using Godot;
 using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 public partial class pause : Control
 {
 	private Control _menu;
+	private TextureButton _button;
 
     public override void _Ready()
     {
         _menu = GetNode<Control>("Menu");
+		_button = GetNode<TextureButton>("ButtonPause");
     }
 
     private void Pause()
 	{
-		GetTree().Paused = !GetTree().Paused;
-		Hide();
+		_button.Hide();
 		_menu.Show();
+		GetTree().Paused = !GetTree().Paused;
+	}
+
+	private void UnPause()
+	{
+		GetTree().Paused = !GetTree().Paused;
+		GD.Print("nevim");
+		_menu.Hide();
+		_button.Show();
 	}
 }
