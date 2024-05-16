7b8ca5d5d91127180c45b7040f1a60862da6989d
_3tHeR_ <135758251+3ther-joyboy@users.noreply.github.com>
2024-05-16 17:00:20 +0200

    lvl editor
    sincing from school (leaving school comit)

--- scripts/lvl_editor.cs
+++ scripts/lvl_editor.cs
@@ -4,6 +4,9 @@ using System;
 public partial class lvl_editor : Control
 {
 	private String _path = "res://scenes/lvl_editor/editor_components/";
+	private String _editedLvl = "user://custom_map.tscn";
+	private PackedScene _editorScene;
+
 	private String[] _objects;
 	private int _mode;
 	private String _selected;
@@ -14,7 +17,19 @@ public partial class lvl_editor : Control
 	}
 
 	public override void _Ready(){
-		
+
+		var lvl = GetNode<Node2D>("lvl");
+		if (FileAccess.FileExists(_editedLvl))
+			_editorScene = ResourceLoader.Load<PackedScene>(_editedLvl);
+			
+		else {
+			PackedScene scene = new PackedScene();
+			scene.Pack(lvl);
+		}	
+
+
+
+
 		_mode = (int)Mode.ObjectSpawning;
 
 		_objects = DirAccess.GetFilesAt(_path);
@@ -40,6 +55,22 @@ public partial class lvl_editor : Control
 
 		}
 
+	}
+	private void _GoBack() {
+
+//		var lvl = GetNode<Node2D>("lvl");
+//		var scene = new PackedScene();
+//		Error result = scene.Pack(lvl);
+//		if (result == Godot.Error.Ok)
+//		{
+//			Error error = ResourceSaver.Save(scene, _editedLvl);
+//			if (error != Godot.Error.Ok)
+//			{
+//				GD.PushError("An error occurred while saving the scene to disk.");
+//			}
+//		}
+
+		GetTree().ChangeSceneToFile((String)ProjectSettings.GetSetting("application/run/main_scene"));
 	}
 	private void _StartLvl() {
 		GetTree().CallGroup("editor_placeholder", "Save");
