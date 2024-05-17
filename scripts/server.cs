deb5ea61deb3a9e4059219abfc0d9c6f129028d1
l3dnac3k <l3dnac3k@gmail.com>
2024-05-17 16:36:22 +0200

    functional player connection to server

--- scripts/server.cs
+++ scripts/server.cs
@@ -3,14 +3,34 @@ using System;
 
 public partial class server : Node
 {
+	// config data
 	public static ConfigFile conf = new ConfigFile();
 	public static int user_id;
 	public static string user_name;
-	public static string default_ip = "141.145.207.166";
 	public static string custom_ip;
+
+	// server data
+	// public static string default_ip = "141.145.207.166";
+	public static string default_ip = "127.0.0.1";
 	public static int port = 25565;
 
+	// client
+	private ENetMultiplayerPeer _peer;
+	private int _connection_id;
+
 	public override void _Ready()
+	{
+		LoadConfig();
+
+		Multiplayer.PeerConnected += PlayerConnected;
+		Multiplayer.PeerDisconnected += PlayerDisconnected;
+		Multiplayer.ConnectedToServer += ConnectionSuccessful;
+		Multiplayer.ConnectionFailed += ConnectionFailed;
+
+		ConnectToServer();
+	}
+
+	public void LoadConfig()
 	{
 		var err = conf.Load("user://config.ini");
 
@@ -26,9 +46,35 @@ public partial class server : Node
 			conf.Save("user://config.ini");
 		}
 
-		// load config
+		// load config data
 		user_id = (int) conf.GetValue("Player", "id");
 		user_name = (string) conf.GetValue("Player", "username");
 		custom_ip = (string) conf.GetValue("Server", "ip");
+	}
+
+	private void ConnectToServer()
+	{
+		_peer = new ENetMultiplayerPeer();
+		var status = _peer.CreateClient(default_ip, port);
+
+		if (status != Error.Ok)
+		{
+			GD.PrintErr("could not create client");
+			return;
+		}
+
+		_peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+		Multiplayer.MultiplayerPeer = _peer;
+	}
+
+	private void PlayerConnected(long id) { GD.Print("player connected: "); }
+
+	private void PlayerDisconnected(long id) { GD.Print("player disconnected"); }
+	private void ConnectionFailed() { GD.Print("could not connect to the server"); }
+
+	private void ConnectionSuccessful()
+	{
+		_connection_id = Multiplayer.GetUniqueId();
+		GD.Print("id: " + _connection_id);
 	}
 }
