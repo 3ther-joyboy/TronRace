1e8821b08f5cf172495d84660316d0ece2f11dd5
_3tHeR_ <135758251+3ther-joyboy@users.noreply.github.com>
2024-05-23 23:01:33 +0200

    mobile rested fix?

--- scripts/short_cuts.cs
+++ scripts/short_cuts.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class short_cuts : Node
 {
-	public override void _UnhandledInput(InputEvent @event){
+	public override void _Input(InputEvent @event){
 
 		if (@event is InputEventKey eventKey) {
 			if (eventKey.Keycode == (Key)4194336) // f5 tho idk how to acces Key. object
